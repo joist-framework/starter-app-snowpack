a61af2c65d76e21fc6f2373993603dc448dd1cb0
dblue <dblue@deloitte.com>
2020-07-31 14:22:23 -0400

    testing-karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,22 +1,14 @@
-const esm = require('@open-wc/karma-esm');
+const { createDefaultConfig } = require('@open-wc/testing-karma');
 
 process.env.CHROME_BIN = require('puppeteer').executablePath();
 
-module.exports = function (config) {
-  config.set({
-    files: [{ pattern: 'target/**/*.spec.js', type: 'module' }],
-    plugins: [esm, 'karma-*'],
-    frameworks: ['jasmine', 'esm'],
-    esm: {
-      nodeResolve: true,
-    },
-    exclude: [],
-    reporters: ['spec'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    browsers: ['ChromeHeadless'],
-    singleRun: true,
-    concurrency: Infinity,
-  });
+module.exports = (config) => {
+  const options = createDefaultConfig(config);
+
+  options.frameworks = ['jasmine', ...options.frameworks];
+  options.files = [{ pattern: 'target/**/*.spec.js', type: 'module' }];
+  options.browsers = ['ChromeHeadless'];
+  options.esm = { nodeResolve: true };
+
+  config.set(options);
 };
