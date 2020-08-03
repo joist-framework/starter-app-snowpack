da01676b247cece7830249c6cf239e3a2048f247
dblue <dblue@deloitte.com>
2020-08-03 15:53:54 -0400

    more simple

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,7 @@
-import { terser } from 'rollup-plugin-terser';
-import minifyHTML from 'rollup-plugin-minify-html-literals';
-
 export default {
   input: 'build/target/main.js',
   output: {
     dir: 'public/target',
     format: 'esm',
   },
-  plugins: [terser(), minifyHTML()],
 };
