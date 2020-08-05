460f10ca604cf18fa9ec99c2d83ee40311eed245
dblue <dblue@deloitte.com>
2020-08-05 10:51:28 -0400

    plugin

--- rollup.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  input: 'build/target/main.js',
-  output: {
-    dir: 'public/target',
-    format: 'esm',
-  },
-};
---
+++ tools/bundle.js
@@ -0,0 +1,33 @@
+// Will be removed once snowpack rollup plugin is released
+
+const { rollup } = require('rollup');
+const { readFile, writeFile } = require('fs');
+const { promisify } = require('util');
+
+const read = promisify(readFile);
+const write = promisify(writeFile);
+
+module.exports = function (snowpackConfig, _pluginOptions) {
+  return {
+    name: 'bundle',
+    async optimize({ buildDirectory }) {
+      const html = await read(`${buildDirectory}/index.html`);
+
+      await write(
+        `${buildDirectory}/index.html`,
+        html.toString().replace(snowpackConfig.mount['src/'], '/js')
+      );
+
+      const bundle = await rollup({
+        input: `${buildDirectory}${snowpackConfig.mount['src/']}/main.js`,
+      });
+
+      return bundle.write({
+        output: {
+          dir: `${buildDirectory}/js`,
+          format: 'esm',
+        },
+      });
+    },
+  };
+};
