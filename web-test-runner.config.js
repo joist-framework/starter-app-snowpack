c1b475a275378e7531034ac818457bd02de7f93e
dblue <dblue@deloitte.com>
2020-09-02 16:04:17 -0400

    test

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,6 +1,7 @@
 const { esbuildPlugin } = require('@web/dev-server-esbuild');
 
 module.exports = {
+  files: ['src/**/*.spec.ts'],
   nodeResolve: true,
   puppeteer: true,
   plugins: [esbuildPlugin({ ts: true })],
